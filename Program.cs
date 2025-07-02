c528a2ed98618c0ac62925e273e529b98cca33a7
rayyan2907 <mrayyan5296@gmail.com>
2025-07-02 11:58:02 +0500

    allowed cors

--- Program.cs
+++ Program.cs
@@ -69,17 +69,11 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins(
-            "http://localhost:5173",
-            "https://whatsapp-react-c3s9-h7p8d7y1j-rayyan2907s-projects.vercel.app",
-            "https://whatsapp-react-c3s9-bu6kwln7q-rayyan2907s-projects.vercel.app",
-            "https://whatsapp-react-c3s9-rayyan2907s-projects.vercel.app",
-            "https://clonechatapp.netlify.app",
-            "http://localhost:63774"
-
-        )
-        .AllowAnyHeader()
-        .AllowAnyMethod();
+        policy.AllowAnyHeader()
+              .AllowAnyMethod()
+              .SetIsOriginAllowed(origin => true) // allows all localhost ports
+              .AllowCredentials();
+      
     });
 });
 
