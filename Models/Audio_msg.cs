7acc457aa3d5a16dd71c1ba2210c07abcb861260
rayyan2907 <mrayyan5296@gmail.com>
2025-07-05 18:15:41 +0500

    debugging

--- Models/Audio_msg.cs
+++ Models/Audio_msg.cs
@@ -2,7 +2,7 @@
 {
     public class Audio_msg : Message_model
     {
-        public IFormFile voice {  get; set; }
+        public IFormFile? voice {  get; set; }
         public string? voice_url { get; set; }
         public string? duration { get; set; }
         public string? voice_byte { get; set; }
