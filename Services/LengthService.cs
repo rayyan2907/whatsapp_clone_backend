32cb05a70b47178cf6d21f9ee916320236d4cfa1
rayyan2907 <mrayyan5296@gmail.com>
2025-07-05 18:29:07 +0500

    solved the issue

--- Services/LengthService.cs
+++ Services/LengthService.cs
@@ -1,46 +1,14 @@
 ﻿using MediaToolkit;
 using MediaToolkit.Model;
-using Xabe.FFmpeg;
+
+
 using System.IO;
 
 namespace whatsapp_clone_backend.Services
 {
     public static class LengthService
     {
-        public static async Task<string> GetAudioDuration(IFormFile audio)
-        {
-            var path = Path.Combine(Directory.GetCurrentDirectory(), "ffmpeg");
-            FFmpeg.SetExecutablesPath(path);
-
-            var tempFilePath = Path.Combine(Path.GetTempPath(), audio.FileName);
-            try
-            {
-                using (var stream = new FileStream(tempFilePath, FileMode.Create))
-                {
-                    await audio.CopyToAsync(stream);
-                }
-
-                var mediaInfo = await FFmpeg.GetMediaInfo(tempFilePath);
-                var audioStream = mediaInfo.AudioStreams.FirstOrDefault();
-                if (audioStream != null)
-                {
-                    var duration = audioStream.Duration;
-                    return duration.ToString(@"hh\:mm\:ss");
-                }
-            }
-            finally
-            {
-                // Cleanup
-                if (File.Exists(tempFilePath))
-                {
-                    File.Delete(tempFilePath);
-                }
-            }
-
-            return "00:00:00";
-        }
-
-
+      
         public static string GetVideoDuration(IFormFile video)
         {
             // Save to a temporary file first
